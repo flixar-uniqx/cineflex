09a47e73638554bdc80d8edd7467856b7e2c4ad3
flixar-uniqx <flixar.uniqx@gmail.com>
2026-01-11 20:24:06 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-const SITE_CONFIG = {
-    appName: "StreamX",
-    primaryColor: "#00f260", // Futuristic Green
-    secondaryColor: "#0575E6", // Deep Blue
-    adminPass: "1234" // Simple client-side lock (Basic security)
-};
